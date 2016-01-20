19398ac111c7c6095d53887c6742f1e3a29bf1e1
harrychen <harry@hymarr.com>
2016-01-20 12:28:56 -0800

    update experience models

--- chenjianzhong/models.py
+++ chenjianzhong/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Experience(models.Model):
+	company = models.CharField(max_length=200)
 	title = models.CharField(max_length=200)
 	role = models.CharField(max_length=200)
 	description = models.TextField()
