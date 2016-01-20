ffa590f84ba68ae1f67e9cb916fb3814f609ca30
harrychen <harry@hymarr.com>
2016-01-20 13:18:06 -0800

    start working frontend view

--- chenjianzhong/views.py
+++ chenjianzhong/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Experience
+from .models import Experience, Project
 
 def index(request):
 	return render(request, 'index.html')
@@ -10,8 +10,8 @@ def experiences(request):
 	return render(request, 'experiences.html', {'experience_list': experience_list})
 
 def projects(request):
-	experience_list = Experience.objects.all
-	return render(request, 'experiences.html', {'experience_list': experience_list})
+	project_list = Project.objects.all
+	return render(request, 'projects.html', {'project_list': project_list})
 
 def yearbook(request, year):
 	return render(request, year + '.html')
